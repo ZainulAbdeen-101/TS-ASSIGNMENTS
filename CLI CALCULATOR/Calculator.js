90ec460baa867278fe93c072a6eedaa54658b3c8
ZainulAbdeen-101 <bbadshah691@gmail.com>
2022-11-30 18:46:01 +0500

    remaining

--- CLI CALCULATOR/Calculator.js
+++ CLI CALCULATOR/Calculator.js
@@ -1,3 +1,36 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log('hello world');
+#!usr/bin/env node
+import inquirer from "inquirer";
+async function myCalc() {
+    const Question = await inquirer
+        .prompt([
+        {
+            type: 'list',
+            name: 'operator',
+            message: `start your calculation `,
+            choices: [`"Addition,Subtraction,Multiplication,Division"]\n`]
+        },
+        {
+            type: "number",
+            name: "num1",
+            message: "enter num1"
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "enter num2"
+        }
+    ]);
+    if (Question.operator == "Addition") {
+        console.log(`${Question.num1}+ ${Question.num2}=${Question.num1 + Question.num2} `);
+    }
+    else if (Question.operator == "Subtraction") {
+        console.log(`${Question.num1}- ${Question.num2}=${Question.num1 - Question.num2} `);
+    }
+    else if (Question.operator == "Multiplication") {
+        console.log(`${Question.num1}* ${Question.num2}=${Question.num1 * Question.num2} `);
+    }
+    else if (Question.operator == "Multiplication") {
+        console.log(`${Question.num1}/ ${Question.num2}=${Question.num1 / Question.num2} `);
+    }
+}
+myCalc();
--- CLI CALCULATOR/Calculator.ts
+++ CLI CALCULATOR/Calculator.ts
@@ -1,3 +1,46 @@
 #!usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from 'chalk'
+import chalkAnimation from 'chalk-animation'
+
+
+async function myCalc(){
+    const Question= await inquirer
+    .prompt([
+        {
+        type:'list',
+        name:'operator',
+        message:`start your calculation? \n`,
+        choices: ["Addition,Subtraction,Multiplication,Division"],
+    },
+    {
+        type:"number",
+        name:"num1",
+        message:"enter num1"
+    },
+    {
+        type:"number",
+        name:"num2",
+        message:"enter num2"
+    }
+
+]);
+
+
+if(Question.operator=="Addition"){
+    console.log(`${Question.num1}+ ${Question.num2}=${Question.num1 +Question.num2} `)
+}else if(
+    Question.operator=="Subtraction"
+){
+    console.log(`${Question.num1}- ${Question.num2}=${Question.num1 -Question.num2} `)
+}else if( Question.operator=="Multiplication"){
+  console.log( `${Question.num1}* ${Question.num2}=${Question.num1 *Question.num2} `);
+   
+}else if( Question.operator=="Multiplication"){
+    console.log( `${Question.num1}/ ${Question.num2}=${Question.num1 /Question.num2} `);
+}
+
+
+}
+myCalc()
